b2a8688faa232417aa961bf401d2d64c1fbe7e69
melp <huzi.master@outlook.com>
2024-10-22 20:53:19 +0800

    purescript modde.

--- modules/haskell-packages.el
+++ modules/haskell-packages.el
@@ -1,3 +1,7 @@
 (add-to-list 'package-selected-packages 'haskell-mode)
 
+(add-to-list 'package-selected-packages 'purescript-mode)
+(add-to-list 'package-selected-packages 'psc-ide)
+
+
 (provide 'haskell-packages)
